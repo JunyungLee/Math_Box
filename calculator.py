e415a53c8dfb2ea026cc4b6a50b12b061998cc9d
JunyungLee <skyjn011@gmail.com>
2025-01-02 01:01:56 +0900

    Update all

--- calculator.py
+++ calculator.py
@@ -5,17 +5,12 @@ def subtract(a, b):
 	return a-b
 def multiply(a, b):
 	return a*b
-<<<<<<< HEAD
-	
-def divide_premium(a, b):
-    return a/b
 
 def getMedian(a, b):
     return (a+b)/2
-=======
+
 def divide_new(a, b):
 	return a/b
 
 def getRemainder(a, b):
     return a//b
->>>>>>> 63b1200 (Add get_Remainder function)
